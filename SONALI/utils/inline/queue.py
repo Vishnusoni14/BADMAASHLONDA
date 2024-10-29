d643539ec1894ce5b9e91070ade04c7e2ac03bd7
NAINCY <vishnu14sahil25love28@gmail.com>
2024-10-29 10:12:57 +0530

    Update queue.py

--- SONALI/utils/inline/queue.py
+++ SONALI/utils/inline/queue.py
@@ -73,10 +73,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/Alone_xdllll"
+                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/BADMAASHLONDA"
             ),
             InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/frind_x_zone_group"
+                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/+bThODpBz5adiNDJl"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -113,7 +113,7 @@ def queuemarkup(_, vidid, chat_id):
         [
             InlineKeyboardButton(
                 text="๏ ᴍᴏʀᴇ ๏",
-                url="https://t.me/Ur_rishu_143",
+                url="https://t.me/Helpbyvishnu_bot",
             ),
         ],
     ]
